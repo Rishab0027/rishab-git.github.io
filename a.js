e5f538829f90c7662a8798d30f73b79f420fbad2
Rishab0027 <136349923+Rishab0027@users.noreply.github.com>
2025-01-18 10:03:17 +0545

    Add files via upload

---
+++ a.js
@@ -0,0 +1,14 @@
+/*function name(){
+    let a=1;
+    let b=2;
+    sum=a+b
+}
+name();
+*/
+function fullname(anjal,boy){
+    console.log (a,b);
+    console.log(a+b);
+
+}
+boy(1,1);
+anjal(2,3);
